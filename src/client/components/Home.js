eddf62b6ae80e6228c0de5e0b772c0b8691f13c2
maotora <maotoramakweba@live.com>
2018-02-22 13:33:53 +0300

    Initial commit - Working SSR

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div> I'm a comma. </div>
+            <button onClick = {() => console.log('hello')}> Press Me! </button>
+        </div>
+    )
+}
+
+export default Home
---
+++ webpack.client.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/client/index.js',
+
+    output: {
+        filename: 'scripts.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', {targets: {browsers: 'last 2 versions'}}],
+                    ]
+                }
+            }
+        ],
+    }
+}
+
